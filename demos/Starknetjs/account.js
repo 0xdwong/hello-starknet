f9baf42d9763182f3da9c9a2f14f9d113916bb1b
0xdwong <0xdwong@gmail.com>
2024-11-14 17:52:08 +0800

    add deploy account demo

--- demos/Starknetjs/account.js
+++ demos/Starknetjs/account.js
@@ -1,26 +1,56 @@
 require('dotenv').config();
-const { RpcProvider, constants, ec, stark, RpcProvider, hash, CallData } = require('starknet');
+const { ec, stark, hash, CallData, Account, RpcProvider, constants, getChecksumAddress } = require('starknet');
 
-function createAccount() {
-    const provider = new RpcProvider({ nodeUrl: constants.NetworkName.SN_SEPOLIA });
 
-    // new Open Zeppelin account v0.8.1
-    // Generate public and private key pair.
-    const privateKey = stark.randomAddress();
-    console.log('New OZ account:\nprivateKey=', privateKey);
-    const starkKeyPub = ec.starkCurve.getStarkKey(privateKey);
-    console.log('publicKey=', starkKeyPub);
+async function createArgentAccount() {
+    let privateKey = stark.randomAddress();
 
-    const OZaccountClassHash = '0x061dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f';
-    // Calculate future address of the account
-    const OZaccountConstructorCallData = CallData.compile({ publicKey: starkKeyPub });
-    const OZcontractAddress = hash.calculateContractAddressFromHash(
-        starkKeyPub,
-        OZaccountClassHash,
-        OZaccountConstructorCallData,
+    const publicKey = ec.starkCurve.getStarkKey(privateKey);
+
+    const constructorCallData = _constructArgentXAccountData(publicKey);
+
+    const accountClassHash = process.env.ArgentXAccountClassHash;
+
+    // calculate address
+    const contractAddress = getChecksumAddress(hash.calculateContractAddressFromHash(
+        publicKey,
+        accountClassHash,
+        constructorCallData,
         0
-    );
-    console.log('Precalculated account address=', OZcontractAddress);
+    ));
+
+    console.log('privateKey=', privateKey);
+    console.log('publicKey=', publicKey);
+    console.log('address=', contractAddress);
+
+    // deploy
+    const nodeUrl = process.env.PROVIDER_URL || constants.NetworkName.SN_SEPOLIA;
+    const provider = new RpcProvider({ nodeUrl });
+    const account = new Account(provider, contractAddress, privateKey);
+
+    const payload = {
+        'classHash': accountClassHash,
+        'constructorCalldata': constructorCallData,
+        'contractAddress': contractAddress,
+        'addressSalt': publicKey,
+    };
+
+    const { transaction_hash, contract_address } = await account.deployAccount(payload);
+    console.log('✅ wallet deployed at:', contract_address);
+    console.log('deploy transaction hash:', transaction_hash);
 }
 
-createAccount();
+function _constructArgentXAccountData(publicKey) {
+    const constructorCallData = CallData.compile({
+        owner: publicKey,
+        guardian: '0',
+    });
+
+    return constructorCallData;
+}
+
+async function main() {
+    await createArgentAccount();
+}
+
+main();
