787bde10d54857cac64c1fcd458e768e5222c05c
0xdwong <0xdwong@gmail.com>
2024-11-15 18:07:25 +0800

    multi type of account

--- demos/Starknetjs/account.js
+++ demos/Starknetjs/account.js
@@ -1,15 +1,41 @@
 require('dotenv').config();
 const { ec, stark, hash, CallData, Account, RpcProvider, constants, getChecksumAddress } = require('starknet');
+const { ArgentXAccountClassHash, OZAccountClassHash } = require('./constants');
+
 
 
 async function createArgentAccount() {
     let privateKey = stark.randomAddress();
+    privateKey = process.env.PRIVATE_KEY
 
+    const type = 'argent';
+
+    return await _createAccount(privateKey, type);
+}
+
+async function createOZAccount() {
+    let privateKey = stark.randomAddress();
+    privateKey = process.env.PRIVATE_KEY
+
+    const type = 'oz';
+
+    return await _createAccount(privateKey, type);
+}
+
+async function _createAccount(privateKey, type) {
     const publicKey = ec.starkCurve.getStarkKey(privateKey);
 
-    const constructorCallData = _constructArgentXAccountData(publicKey);
+    const constructorCallData = _getConstructorCalldata(publicKey, type);
+    if (!constructorCallData) {
+        console.log('not supported type');
+        return;
+    }
 
-    const accountClassHash = process.env.ArgentXAccountClassHash;
+    const accountClassHash = _accountClassHash(type);
+    if (!constructorCallData) {
+        console.log('not account hash');
+        return;
+    }
 
     // calculate address
     const contractAddress = getChecksumAddress(hash.calculateContractAddressFromHash(
@@ -24,20 +50,53 @@ async function createArgentAccount() {
     console.log('address=', contractAddress);
 
     // deploy
-    const nodeUrl = process.env.PROVIDER_URL || constants.NetworkName.SN_SEPOLIA;
-    const provider = new RpcProvider({ nodeUrl });
-    const account = new Account(provider, contractAddress, privateKey);
+    // const nodeUrl = process.env.PROVIDER_URL || constants.NetworkName.SN_SEPOLIA;
+    // const provider = new RpcProvider({ nodeUrl });
+    // const account = new Account(provider, contractAddress, privateKey);
 
-    const payload = {
-        'classHash': accountClassHash,
-        'constructorCalldata': constructorCallData,
-        'contractAddress': contractAddress,
-        'addressSalt': publicKey,
-    };
+    // const payload = {
+    //     'classHash': accountClassHash,
+    //     'constructorCalldata': constructorCallData,
+    //     'contractAddress': contractAddress,
+    //     'addressSalt': publicKey,
+    // };
 
-    const { transaction_hash, contract_address } = await account.deployAccount(payload);
-    console.log('✅ wallet deployed at:', contract_address);
-    console.log('deploy transaction hash:', transaction_hash);
+    // const { transaction_hash, contract_address } = await account.deployAccount(payload);
+    // console.log('✅ wallet deployed at:', contract_address);
+    // console.log('deploy transaction hash:', transaction_hash);
+}
+
+function _accountClassHash(type) {
+    let classHash;
+    switch (type) {
+        case 'argent':
+            classHash = ArgentXAccountClassHash;
+            break;
+        case 'oz':
+            classHash = OZAccountClassHash;
+            break;
+        default:
+            break;
+    }
+
+    return classHash;
+}
+
+
+function _getConstructorCalldata(publicKey, type) {
+    let constructorCalldata;
+    switch (type) {
+        case 'argent':
+            constructorCalldata = _constructArgentXAccountData(publicKey);
+            break;
+        case 'oz':
+            constructorCalldata = _constructOZAccountData(publicKey);
+            break;
+        default:
+            break;
+    }
+
+    return constructorCalldata;
 }
 
 function _constructArgentXAccountData(publicKey) {
@@ -49,8 +108,18 @@ function _constructArgentXAccountData(publicKey) {
     return constructorCallData;
 }
 
+function _constructOZAccountData(publicKey) {
+    const constructorCallData = CallData.compile({
+        publicKey: publicKey
+    });
+
+    return constructorCallData;
+}
+
 async function main() {
     await createArgentAccount();
+
+    // await createOZAccount();
 }
 
 main();
--- demos/Starknetjs/constants.js
+++ demos/Starknetjs/constants.js
@@ -1,7 +1,13 @@
 const ETH_ADDRESS = '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7';
 const STARK_ADDRESS = '0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d';
 
+const OZAccountClassHash = "0x061dac032f228abef9c6626f995015233097ae253a7f72d68552db02f2971b8f"
+const ArgentXAccountClassHash = "0x1a736d6ed154502257f02b1ccdf4d9d1089f80811cd6acad48e6b6a9d1f2003"
+
+
 module.exports = {
     ETH_ADDRESS,
     STARK_ADDRESS,
+    OZAccountClassHash,
+    ArgentXAccountClassHash,
 };
